9a20b7aae3aaabe36e736b48dce98f449c880cfe
detzki <detzki@detzki.ovh>
2026-02-10 10:26:38 +0100

    change permissions on key and cert

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -276,6 +276,16 @@ class wazuh::agent (
         ensure => "${agent_package_version}-${agent_package_revision}", # lint:ignore:security_package_pinned_version
         require=>File['wazuh-source']
       }->  notify { "Checking if packages is installed before apt update gets resolved": loglevel => "info" }
+      [ "key", "crt"].each | String $cert_or_key | {
+      file { "/var/ossec/etc/${hostname}.$cert_or_key":
+        ensure  => file,
+        owner   => 'wazuh',
+        group   => 'wazuh',
+        mode    => '0644',
+        require=>Package["$agent_package_name"]
+        }
+      }
+
     }
     'windows': {
       file { $download_path:
