0e8579befd80d39e0e1623e11d9c0a60d75bbe38
detzki <detzki@detzki.ovh>
2026-03-04 14:52:50 +0100

    remove debug info

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -275,7 +275,7 @@ class wazuh::agent (
         package { $agent_package_name:
         ensure => "${agent_package_version}-${agent_package_revision}", # lint:ignore:security_package_pinned_version
         require=>File['wazuh-source']
-      }->  notify { "Checking if packages is installed before apt update gets resolved": loglevel => "info" }
+      }
 
 
     }
